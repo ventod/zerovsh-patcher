4e7a3f970412afe5966db0854bc37e108a5732af
Vento <nope@mail.com>
2022-04-26 14:57:40 +0200

    USE_EF0 flag handling inside build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
 cd kernel
 make clean
-make DEBUG=0
+make DEBUG=0 USE_EF0=0
 cp zerovsh_patcher.prx ../bin
 rm -Rf *.prx *.elf
 cd ../
--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,6 @@
 cd kernel
 make clean
-make DEBUG=0
+make DEBUG=0 USE_EF0=0
 copy zerovsh_patcher.prx ../bin
 del -Rf *.prx *.elf zerovsh_upatcher.h
 cd ../
