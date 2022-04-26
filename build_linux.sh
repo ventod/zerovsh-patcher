63f6221117df73211009e6c9bd0ee4d73a29931f
Vento <nope@mail.com>
2022-04-26 15:00:16 +0200

    Build both ef0 and ms0 plugins

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,13 @@
 cd kernel
+
 make clean
 make DEBUG=0 USE_EF0=0
-cp zerovsh_patcher.prx ../bin
+cp zerovsh_patcher.prx ../bin/zerovsh_patcher_ms0.prx
 rm -Rf *.prx *.elf
+
+make clean
+make DEBUG=0 USE_EF0=1
+cp zerovsh_patcher.prx ../bin/zerovsh_patcher_ef0.prx
+rm -Rf *.prx *.elf
+
 cd ../
--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,13 @@
 cd kernel
+
 make clean
 make DEBUG=0 USE_EF0=0
-copy zerovsh_patcher.prx ../bin
-del -Rf *.prx *.elf zerovsh_upatcher.h
+copy zerovsh_patcher.prx ../bin/zerovsh_patcher_ms0.prx
+del -Rf *.prx *.elf
+
+make clean
+make DEBUG=0 USE_EF0=1
+copy zerovsh_patcher.prx ../bin/zerovsh_patcher_ef0.prx
+del -Rf *.prx *.elf
+
 cd ../
